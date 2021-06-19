06d093166bad7856bab6ab87fb797cbb756c717f
Waheed Nazir <mwaheednazir8@gmail.com>
2021-06-20 06:27:52 +0800

    - Dagger Base Activity moved to Hilt, Cleaned up dagger code.

--- app/src/main/java/com/kotlin/mvvm/ui/DaggerActivity.kt
+++ app/src/main/java/com/kotlin/mvvm/ui/DaggerActivity.kt
@@ -1,23 +1,10 @@
 package com.kotlin.mvvm.ui
 
-import androidx.lifecycle.ViewModelProvider
-import dagger.android.support.DaggerAppCompatActivity
-import javax.inject.Inject
+import androidx.appcompat.app.AppCompatActivity
 
 /**
  * Created by Waheed on 04,November,2019
  * Updated to dagger 2.27, 29, September 2020
  */
 
-// Referring this class as BaseActivity
-
-typealias BaseActivity = DaggerActivity
-
-/**
- * Activity providing Dagger support and [ViewModel] support
- */
-abstract class DaggerActivity : DaggerAppCompatActivity() {
-
-    @Inject
-    lateinit var viewModelFactory: ViewModelProvider.Factory
-}
+abstract class BaseActivity : AppCompatActivity()
