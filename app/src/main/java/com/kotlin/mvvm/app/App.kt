57c8ae3c7c79b11d5cf2facf8f9d47a5f6031308
Waheed Nazir <mwaheednazir8@gmail.com>
2021-06-20 06:25:46 +0800

    - Application class migrated to Hilt

--- app/src/main/java/com/kotlin/mvvm/app/App.kt
+++ app/src/main/java/com/kotlin/mvvm/app/App.kt
@@ -1,17 +1,12 @@
 package com.kotlin.mvvm.app
 
-import com.kotlin.mvvm.di.components.DaggerAppComponent
-import dagger.android.AndroidInjector
-import dagger.android.support.DaggerApplication
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
 
 /**
  * Created by Waheed on 04,November,2019
+ * Migrated to Hilt 20, June, 2021
  */
 
-class App : DaggerApplication() {
-
-    private val applicationInjector = DaggerAppComponent.builder().application(this).build()
-
-    override fun applicationInjector(): AndroidInjector<out DaggerApplication> = applicationInjector
-
-}
+@HiltAndroidApp
+open class App : Application()
